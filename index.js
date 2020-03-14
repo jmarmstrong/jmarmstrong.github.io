d84b6dfbe870fbd0c4099b1c88c1928d9f742858
Joseph Armstrong <joarmstrong2014@gmail.com>
2020-03-14 15:54:25 +0000

    added index.js and edited html

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: 'Joseph Armstrong',
+        projects: [
+            {
+                projectname: azerty - the - discord - bot
+            }
+        ]
+    }
+})
